eb6cd1a9268148b3c0269e78635a3892789da9f1
Aleksandra Strojwąs <strojwas@MacBook-Pro-Aleksandra.local>
2026-01-26 22:41:39 +0100

    Remove conflict in urls.py

--- core/urls.py
+++ core/urls.py
@@ -1,17 +1,5 @@
 """
 URL configuration for core project.
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/5.2/topics/http/urls/
-Examples:
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
 from django.urls import path
@@ -25,17 +13,17 @@ urlpatterns = [
     path('logout/', auth_views.LogoutView.as_view(), name='logout'),
 
     path('home/', views.home, name='home'),
-<<<<<<< Updated upstream
-    path('patient/<int:pk>', views.patient_detail, name='patient_detail'),
-    path('add_patient/',views.add_patient,name='add_patient'),
-    path('appointments/', views.appointments, name='appointments'),
-]
-=======
 
+    # Patient detail
     path('patient/<int:pk>/', views.patient_detail, name='patient_detail'),
 
+    # Delete patient
     path('patient/<int:pk>/delete/', views.delete_patient, name='delete_patient'),
 
+    # Add patient
     path('add_patient/', views.add_patient, name='add_patient'),
+
+    # Appointments
+    path('appointments/', views.appointments, name='appointments'),
 ]
->>>>>>> Stashed changes
+
